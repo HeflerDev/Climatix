8f6fe4aeba04b6e9d16e93d982dc48e189d396eb
HeflerDev <hheflerdevelopment@gmail.com>
2020-10-12 19:26:15 -0300

    struct/basic

---
+++ src/controllers/displayController.js
@@ -0,0 +1,9 @@
+const displayController = (() => {
+
+
+
+
+    return { };
+})();
+
+export default displayController;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './css/master.scss';
+
+
+
+/* async function doWork() {
+ *  try {
+ *      something
+ *  } catch (err) {
+ *      console.log(err);
+ *  }
+ * }
+
+/*
+fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=72317f5668bade497a7edbd246f2df82')
+    .then(res => {
+        if (res.ok) {
+            // Success
+        } else {
+            // Failure
+        }
+    })
+    .then(data => console.log(data))
+    // .catch(console.log('ERROR'))
+//
+//
+*/
